79138a8969e13e7da7a788acb6ae6842544e19f4
origogi <rlawjdxo1212@naver.com>
2022-01-17 21:25:48 +0900

    Add searchbar

--- src/Header/Searcher.js
+++ src/Header/Searcher.js
@@ -1,12 +1,27 @@
-import React from 'react'
-import "./Searcher.css"
+import { KeyboardOutlined, MicOutlined, Search, SearchOutlined } from "@mui/icons-material";
+import { Divider } from "@mui/material";
+import React from "react";
+import "./Searcher.css";
 
 function Searcher() {
-    return (
-        <div>
-            <input placeholder="검색" />
-        </div>
-    )
+  return (
+    <div className="searcher">
+      <div className="seacher_input">
+        <input placeholder="검색" />
+        <KeyboardOutlined className="seacher_input_keyboard" sx={{ fontSize: 20 }} />
+        
+        <Divider orientation="vertical" />
+
+        <div className="searcher_button">
+          <SearchOutlined />
+      </div>
+      </div>
+
+      <div className="searcher_mic">
+        <MicOutlined sx={{ fontSize: 20 }} />
+      </div>
+    </div>
+  );
 }
 
-export default Searcher
+export default Searcher;
