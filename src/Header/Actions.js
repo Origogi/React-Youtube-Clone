6d263a6f91a68e8496f94088830719f718c20e56
origogi <rlawjdxo1212@naver.com>
2022-01-17 20:44:18 +0900

    add actions bar

--- src/Header/Actions.js
+++ src/Header/Actions.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import './Actions.css'
-import { VideoCallOutlined } from '@mui/icons-material';
+import {VideoCallOutlined, NotificationsNoneOutlined, ViewModuleOutlined } from '@mui/icons-material';
 import { Avatar } from '@mui/material';
 
 function Actions() {
     return (
         <div className='actions'>
             <VideoCallOutlined />
-            <VideoCallOutlined />
-            <VideoCallOutlined />
+            <ViewModuleOutlined />
+            <NotificationsNoneOutlined />
             <Avatar 
                 src='https://yt3.ggpht.com/yti/APfAmoFK0nBeTNqtTweNKKREgyYOjILqudmrc2yKfA=s88-c-k-c0x00ffffff-no-rj-mo'
+                sx={{ width: 32, height: 32 }}
             />
         </div>
     )
