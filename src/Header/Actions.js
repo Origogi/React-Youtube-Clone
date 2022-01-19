afe19b0ee7f724f2ad281d18b30842693f6ea652
origogi <rlawjdxo1212@naver.com>
2022-01-19 19:14:30 +0900

    add action icon hover

--- src/Header/Actions.js
+++ src/Header/Actions.js
@@ -1,20 +1,25 @@
-import React from 'react'
-import './Actions.css'
-import {VideoCallOutlined, NotificationsNoneOutlined, ViewModuleOutlined } from '@mui/icons-material';
-import { Avatar } from '@mui/material';
+import React from "react";
+import "./Actions.css";
+import {
+  VideoCallOutlined,
+  NotificationsNoneOutlined,
+  ViewModuleOutlined,
+} from "@mui/icons-material";
+import { Avatar } from "@mui/material";
 
 function Actions() {
-    return (
-        <div className='actions'>
-            <VideoCallOutlined />
-            <ViewModuleOutlined />
-            <NotificationsNoneOutlined />
-            <Avatar 
-                src='https://yt3.ggpht.com/yti/APfAmoFK0nBeTNqtTweNKKREgyYOjILqudmrc2yKfA=s88-c-k-c0x00ffffff-no-rj-mo'
-                sx={{ width: 32, height: 32 }}
-            />
-        </div>
-    )
+  return (
+    <div className="actions">
+      <VideoCallOutlined className="actions_icon" />
+      <ViewModuleOutlined className="actions_icon" />
+      <NotificationsNoneOutlined className="actions_icon" />
+      <Avatar
+        className="actions_icon"
+        src="https://yt3.ggpht.com/yti/APfAmoFK0nBeTNqtTweNKKREgyYOjILqudmrc2yKfA=s88-c-k-c0x00ffffff-no-rj-mo"
+        sx={{ width: 32, height: 32 }}
+      />
+    </div>
+  );
 }
 
-export default Actions
+export default Actions;
