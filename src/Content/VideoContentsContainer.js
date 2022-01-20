b7aaa5535b3cabad433aae56b85bcbcfc0d9cdf5
origogi <rlawjdxo1212@naver.com>
2022-01-20 19:23:02 +0900

    Adjust flex-wrap

--- src/Content/VideoContentsContainer.js
+++ src/Content/VideoContentsContainer.js
@@ -1,13 +1,32 @@
-import React from 'react'
-import VideoContainer from './VideoContainer'
-import './VideoContentsContainer.css'
+import React from "react";
+import VideoContainer from "./VideoContainer";
+import "./VideoContentsContainer.css";
 
 function VideocontentsContainer() {
-    return (
-        <div className='videoContentsContainer'>
-            <VideoContainer imgSrc={'https://i.ytimg.com/vi/KHXosa22McU/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAN9dN6AQK04L2hb-5T25vrwbwkLw'}/>
-        </div>
-    )
+  return (
+    <div className="videoContentsContainer">
+      <VideoContainer
+        imgSrc={
+          "https://i.ytimg.com/vi/KHXosa22McU/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAN9dN6AQK04L2hb-5T25vrwbwkLw"
+        }
+      />
+      <VideoContainer
+        imgSrc={
+          "https://i.ytimg.com/vi/KHXosa22McU/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAN9dN6AQK04L2hb-5T25vrwbwkLw"
+        }
+      />
+      <VideoContainer
+        imgSrc={
+          "https://i.ytimg.com/vi/KHXosa22McU/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAN9dN6AQK04L2hb-5T25vrwbwkLw"
+        }
+      />
+      <VideoContainer
+        imgSrc={
+          "https://i.ytimg.com/vi/KHXosa22McU/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAN9dN6AQK04L2hb-5T25vrwbwkLw"
+        }
+      />
+    </div>
+  );
 }
 
-export default VideocontentsContainer
+export default VideocontentsContainer;
