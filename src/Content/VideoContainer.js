603e05e080eb248f0c29a1e042e74228ca38d319
origogi <rlawjdxo1212@naver.com>
2022-01-21 19:38:07 +0900

    add title

--- src/Content/VideoContainer.js
+++ src/Content/VideoContainer.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import './VideoContainer.css'
+import { Avatar } from "@mui/material";
+import React from "react";
+import "./VideoContainer.css";
 
-function VideoContainer({ imgSrc }) {
-  return <div className='videoContainer'>
-      <img className = 'videoContainer_thumbnail' src = { imgSrc }/>
-  </div>;
+function VideoContainer({ imgSrc, avatarSrc, title }) {
+  return (
+    <div className="videoContainer">
+      <img className="videoContainer_thumbnail" src={imgSrc} />
+      <div className="videoContainer_content">
+        <Avatar
+          src={ avatarSrc }
+          sx={{ width: 32, height: 32 }}
+        />
+        <div className="videoContainer_content_desc">
+          <h5>{ title }</h5>
+          <h6>비디오 설명</h6>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default VideoContainer;
--- src/Content/VideoContentsContainer.js
+++ src/Content/VideoContentsContainer.js
@@ -6,23 +6,47 @@ function VideocontentsContainer() {
   return (
     <div className="videoContentsContainer">
       <VideoContainer
+        avatarSrc={
+          "https://yt3.ggpht.com/ytc/AKedOLTWy5S-BJSafYXlfX-5Cj0C5ztuAHZ8zR2Ncls8qg=s68-c-k-c0x00ffffff-no-rj"
+        }
         imgSrc={
           "https://i.ytimg.com/vi/KHXosa22McU/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAN9dN6AQK04L2hb-5T25vrwbwkLw"
         }
-      />
-      <VideoContainer
-        imgSrc={
-          "https://i.ytimg.com/vi/KHXosa22McU/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAN9dN6AQK04L2hb-5T25vrwbwkLw"
+        title={
+          "[영화리뷰/결말포함] 스파이더맨1 전설의 시작!/노웨이홈 보기 전 필수 감상(샘스파1 20분 완전정복) "
         }
       />
       <VideoContainer
+        avatarSrc={
+          "https://yt3.ggpht.com/ytc/AKedOLTWy5S-BJSafYXlfX-5Cj0C5ztuAHZ8zR2Ncls8qg=s68-c-k-c0x00ffffff-no-rj"
+        }
         imgSrc={
           "https://i.ytimg.com/vi/KHXosa22McU/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAN9dN6AQK04L2hb-5T25vrwbwkLw"
         }
+        title={
+          "[영화리뷰/결말포함] 스파이더맨1 전설의 시작!/노웨이홈 보기 전 필수 감상(샘스파1 20분 완전정복) "
+        }
       />
       <VideoContainer
+        avatarSrc={
+          "https://yt3.ggpht.com/ytc/AKedOLTWy5S-BJSafYXlfX-5Cj0C5ztuAHZ8zR2Ncls8qg=s68-c-k-c0x00ffffff-no-rj"
+        }
         imgSrc={
           "https://i.ytimg.com/vi/KHXosa22McU/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAN9dN6AQK04L2hb-5T25vrwbwkLw"
+        }
+        title={
+          "[영화리뷰/결말포함] 스파이더맨1 전설의 시작!/노웨이홈 보기 전 필수 감상(샘스파1 20분 완전정복) "
+        }
+      />
+      <VideoContainer
+        avatarSrc={
+          "https://yt3.ggpht.com/ytc/AKedOLTWy5S-BJSafYXlfX-5Cj0C5ztuAHZ8zR2Ncls8qg=s68-c-k-c0x00ffffff-no-rj"
+        }
+        imgSrc={
+          "https://i.ytimg.com/vi/KHXosa22McU/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAN9dN6AQK04L2hb-5T25vrwbwkLw"
+        }
+        title={
+          "[영화리뷰/결말포함] 스파이더맨1 전설의 시작!/노웨이홈 보기 전 필수 감상(샘스파1 20분 완전정복) "
         }
       />
     </div>
