6a4a48a30682d666db242cdfa3bfbcdd9e5a2ccf
origogi <rlawjdxo1212@naver.com>
2022-01-22 22:10:01 +0900

    update dummy data

--- src/Content/VideoContentsContainer.js
+++ src/Content/VideoContentsContainer.js
@@ -15,18 +15,95 @@ function VideocontentsContainer() {
         title={
           "리브 샌박 vs. 광동 | 매치15 하이라이트 | 01.21 | 2022 LCK 스프링 스플릿"
         }
-        subscriber={
-          "LCK"
-        }
-
-        uploadTime={
-          "10분전"
-        }
-        views={
-          "조회수 816회"
-        }
+        subscriber={"LCK"}
+        uploadTime={"10분전"}
+        views={"조회수 816회"}
       />
-            <VideoContainer
+      <VideoContainer
+        avatarSrc={
+          "https://yt3.ggpht.com/Fef_8oLf6u9pS1TEX6a4e12sTRr-IP-XQo26eg63vZizMItQiGrDZgcTJxugtE08216IZn2zNA=s68-c-k-c0x00ffffff-no-rj"
+        }
+        imgSrc={
+          "https://i.ytimg.com/vi/wDfqXR_5yyQ/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLABpZ540QQowrnZQvhdq9hTcAlxpw"
+        }
+        title={
+          "아이유(IU)의 킬링보이스를 라이브로! - 하루 끝, 너의 의미, 스물셋, 밤편지, 팔레트, 가을 아침, 삐삐, Blueming, 에잇, Coin, 라일락 ㅣ 딩고뮤직"
+        }
+        subscriber={"딩고 뮤직 / dingo music"}
+        uploadTime={"9개월 전"}
+        views={"조회수 3653만회"}
+      />
+      <VideoContainer
+        avatarSrc={
+          "https://yt3.ggpht.com/ytc/AKedOLRfd_D7OLG7kY-j4nhX9VD-q4phRC-ErDTVn58gNw=s68-c-k-c0x00ffffff-no-rj"
+        }
+        imgSrc={
+          "https://i.ytimg.com/vi/FpO7m9GsPnk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBO4Pcl0J9KVOPfqx6kulTJlwHHBQ"
+        }
+        title={
+          "고언어 GoLang, 블록체인 Blockchain, 백엔드, 모든 개발자가 꼭 주목해야 하는 프로그래밍 언어 ⭐️"
+        }
+        subscriber={"드림코딩 by 엘리"}
+        uploadTime={"1년 전"}
+        views={"조회수 2.3만회"}
+      />
+      <VideoContainer
+        avatarSrc={
+          "https://yt3.ggpht.com/ytc/AKedOLQzLWW_VAtsqNTLHUnYmMQTnN_5wWAtT2kCdMiY6g=s48-c-k-c0x00ffffff-no-rj"
+        }
+        imgSrc={
+          "https://i.ytimg.com/vi/fD1k_i1Jalg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDNI4zN5iPhfTaBrHKnn8bP3PUjKw"
+        }
+        title={
+          "Spider-Man In Real Life Public Stunt"
+        }
+        subscriber={"Jesse"}
+        uploadTime={"2년 전"}
+        views={"조회수 3786만회"}
+      />
+      <VideoContainer
+        avatarSrc={
+          "https://yt3.ggpht.com/ytc/AKedOLSdpMtP2ALvRjRylieALjyqJzNnhBOotee1X8ne=s68-c-k-c0x00ffffff-no-rj"
+        }
+        imgSrc={
+          "https://i.ytimg.com/vi/ZgGS1nSo8hs/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDh97bwU2ASpSl_bCxcsqGoQ3fEag"
+        }
+        title={
+          "해도해도 너무 비싼 배달음식의 비밀!"
+        }
+        subscriber={"지식한입"}
+        uploadTime={"1개월 전"}
+        views={"조회수 100만회"}
+      />
+      <VideoContainer
+        avatarSrc={
+          "https://yt3.ggpht.com/Xtui8KPgKV12Embc2OelE3IILvmXex37iC4fCmLrJ04IHl5S_eln4J6smWoafQ6gUzL8mEcWRg=s48-c-k-c0x00ffffff-no-rj"
+        }
+        imgSrc={
+          "https://i.ytimg.com/an_webp/xCbKDI654a8/mqdefault_6s.webp?du=3000&sqp=CLPfr48G&rs=AOn4CLD9ojRJMwAIs0bhOf21IOyqBrV_TQ"
+        }
+        title={
+          "원신 | 신규 캐릭터 플레이 - 「신학: 야외에서 들려오는 학의 울음소리」"
+        }
+        subscriber={"원신"}
+        uploadTime={"2주 전"}
+        views={"조회수 18만회"}
+      />
+      <VideoContainer
+        avatarSrc={
+          "https://yt3.ggpht.com/ysiny8H3t3qvLquOhGe21DTq7gmvI6aNcXx26oRcuiQY4MzpxWe1gM-fRuNIqrb-88uyqn-aq-w=s68-c-k-c0x00ffffff-no-rj"
+        }
+        imgSrc={
+          "https://i.ytimg.com/vi/xsKxZzAmiCk/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB6XJ1POX276sIRO9kXwIjRCyyYaA"
+        }
+        title={
+          "카카오브레인이 ICCV 2021 VALUE Challenge에서 우승한 비결은?"
+        }
+        subscriber={"kakaobrain"}
+        uploadTime={"5일 전"}
+        views={"조회수 34회"}
+      />
+      <VideoContainer
         avatarSrc={
           "https://yt3.ggpht.com/lhqOqy7EZrOa9BBe9XKWafeH7kw3kfJy5QtDWdGvuu8tADalm3g2jDveboMu2USuCJqklgiirw=s68-c-k-c0x00ffffff-no-rj"
         }
@@ -36,18 +113,39 @@ function VideocontentsContainer() {
         title={
           "리브 샌박 vs. 광동 | 매치15 하이라이트 | 01.21 | 2022 LCK 스프링 스플릿"
         }
-        subscriber={
-          "LCK"
-        }
-
-        uploadTime={
-          "10분전"
-        }
-        views={
-          "조회수 816회"
-        }
+        subscriber={"LCK"}
+        uploadTime={"10분전"}
+        views={"조회수 816회"}
       />
-            <VideoContainer
+      <VideoContainer
+        avatarSrc={
+          "https://yt3.ggpht.com/Xtui8KPgKV12Embc2OelE3IILvmXex37iC4fCmLrJ04IHl5S_eln4J6smWoafQ6gUzL8mEcWRg=s68-c-k-c0x00ffffff-no-rj"
+        }
+        imgSrc={
+          "https://i.ytimg.com/vi/AG6ShuG52Y0/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDw3ZbKjhRdvCp-eY3gxpTzCHWY-A"
+        }
+        title={
+          "원신｜ EP - 눈부신 기린의 행적"
+        }
+        subscriber={"원신"}
+        uploadTime={"9시간 전"}
+        views={"조회수 3.1만회"}
+      />
+      <VideoContainer
+        avatarSrc={
+          "https://yt3.ggpht.com/ytc/AKedOLQxjW9reD4wtXjJWR1mzH8L37nkc670i8ZHNP8C=s68-c-k-c0x00ffffff-no-rj"
+        }
+        imgSrc={
+          "https://i.ytimg.com/vi/1ttLx9MbrCI/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB07o8-Njfg877kriDevv5MP7buFg"
+        }
+        title={
+          "생활코딩"
+        }
+        subscriber={"생활코딩"}
+        uploadTime={"7년 전"}
+        views={"조회수 46만회"}
+      />
+      <VideoContainer
         avatarSrc={
           "https://yt3.ggpht.com/lhqOqy7EZrOa9BBe9XKWafeH7kw3kfJy5QtDWdGvuu8tADalm3g2jDveboMu2USuCJqklgiirw=s68-c-k-c0x00ffffff-no-rj"
         }
@@ -57,18 +155,11 @@ function VideocontentsContainer() {
         title={
           "리브 샌박 vs. 광동 | 매치15 하이라이트 | 01.21 | 2022 LCK 스프링 스플릿"
         }
-        subscriber={
-          "LCK"
-        }
-
-        uploadTime={
-          "10분전"
-        }
-        views={
-          "조회수 816회"
-        }
+        subscriber={"LCK"}
+        uploadTime={"10분전"}
+        views={"조회수 816회"}
       />
-            <VideoContainer
+      <VideoContainer
         avatarSrc={
           "https://yt3.ggpht.com/lhqOqy7EZrOa9BBe9XKWafeH7kw3kfJy5QtDWdGvuu8tADalm3g2jDveboMu2USuCJqklgiirw=s68-c-k-c0x00ffffff-no-rj"
         }
@@ -78,18 +169,11 @@ function VideocontentsContainer() {
         title={
           "리브 샌박 vs. 광동 | 매치15 하이라이트 | 01.21 | 2022 LCK 스프링 스플릿"
         }
-        subscriber={
-          "LCK"
-        }
-
-        uploadTime={
-          "10분전"
-        }
-        views={
-          "조회수 816회"
-        }
+        subscriber={"LCK"}
+        uploadTime={"10분전"}
+        views={"조회수 816회"}
       />
-            <VideoContainer
+      <VideoContainer
         avatarSrc={
           "https://yt3.ggpht.com/lhqOqy7EZrOa9BBe9XKWafeH7kw3kfJy5QtDWdGvuu8tADalm3g2jDveboMu2USuCJqklgiirw=s68-c-k-c0x00ffffff-no-rj"
         }
@@ -99,18 +183,11 @@ function VideocontentsContainer() {
         title={
           "리브 샌박 vs. 광동 | 매치15 하이라이트 | 01.21 | 2022 LCK 스프링 스플릿"
         }
-        subscriber={
-          "LCK"
-        }
-
-        uploadTime={
-          "10분전"
-        }
-        views={
-          "조회수 816회"
-        }
+        subscriber={"LCK"}
+        uploadTime={"10분전"}
+        views={"조회수 816회"}
       />
-            <VideoContainer
+      <VideoContainer
         avatarSrc={
           "https://yt3.ggpht.com/lhqOqy7EZrOa9BBe9XKWafeH7kw3kfJy5QtDWdGvuu8tADalm3g2jDveboMu2USuCJqklgiirw=s68-c-k-c0x00ffffff-no-rj"
         }
@@ -120,186 +197,10 @@ function VideocontentsContainer() {
         title={
           "리브 샌박 vs. 광동 | 매치15 하이라이트 | 01.21 | 2022 LCK 스프링 스플릿"
         }
-        subscriber={
-          "LCK"
-        }
-
-        uploadTime={
-          "10분전"
-        }
-        views={
-          "조회수 816회"
-        }
+        subscriber={"LCK"}
+        uploadTime={"10분전"}
+        views={"조회수 816회"}
       />
-            <VideoContainer
-        avatarSrc={
-          "https://yt3.ggpht.com/lhqOqy7EZrOa9BBe9XKWafeH7kw3kfJy5QtDWdGvuu8tADalm3g2jDveboMu2USuCJqklgiirw=s68-c-k-c0x00ffffff-no-rj"
-        }
-        imgSrc={
-          "https://i.ytimg.com/vi/Tqjt0atjFv8/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBg7gFAHwYLtUz9d20AswZs7vtdrw"
-        }
-        title={
-          "리브 샌박 vs. 광동 | 매치15 하이라이트 | 01.21 | 2022 LCK 스프링 스플릿"
-        }
-        subscriber={
-          "LCK"
-        }
-
-        uploadTime={
-          "10분전"
-        }
-        views={
-          "조회수 816회"
-        }
-      />
-            <VideoContainer
-        avatarSrc={
-          "https://yt3.ggpht.com/lhqOqy7EZrOa9BBe9XKWafeH7kw3kfJy5QtDWdGvuu8tADalm3g2jDveboMu2USuCJqklgiirw=s68-c-k-c0x00ffffff-no-rj"
-        }
-        imgSrc={
-          "https://i.ytimg.com/vi/Tqjt0atjFv8/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBg7gFAHwYLtUz9d20AswZs7vtdrw"
-        }
-        title={
-          "리브 샌박 vs. 광동 | 매치15 하이라이트 | 01.21 | 2022 LCK 스프링 스플릿"
-        }
-        subscriber={
-          "LCK"
-        }
-
-        uploadTime={
-          "10분전"
-        }
-        views={
-          "조회수 816회"
-        }
-      />
-            <VideoContainer
-        avatarSrc={
-          "https://yt3.ggpht.com/lhqOqy7EZrOa9BBe9XKWafeH7kw3kfJy5QtDWdGvuu8tADalm3g2jDveboMu2USuCJqklgiirw=s68-c-k-c0x00ffffff-no-rj"
-        }
-        imgSrc={
-          "https://i.ytimg.com/vi/Tqjt0atjFv8/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBg7gFAHwYLtUz9d20AswZs7vtdrw"
-        }
-        title={
-          "리브 샌박 vs. 광동 | 매치15 하이라이트 | 01.21 | 2022 LCK 스프링 스플릿"
-        }
-        subscriber={
-          "LCK"
-        }
-
-        uploadTime={
-          "10분전"
-        }
-        views={
-          "조회수 816회"
-        }
-      />
-            <VideoContainer
-        avatarSrc={
-          "https://yt3.ggpht.com/lhqOqy7EZrOa9BBe9XKWafeH7kw3kfJy5QtDWdGvuu8tADalm3g2jDveboMu2USuCJqklgiirw=s68-c-k-c0x00ffffff-no-rj"
-        }
-        imgSrc={
-          "https://i.ytimg.com/vi/Tqjt0atjFv8/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBg7gFAHwYLtUz9d20AswZs7vtdrw"
-        }
-        title={
-          "리브 샌박 vs. 광동 | 매치15 하이라이트 | 01.21 | 2022 LCK 스프링 스플릿"
-        }
-        subscriber={
-          "LCK"
-        }
-
-        uploadTime={
-          "10분전"
-        }
-        views={
-          "조회수 816회"
-        }
-      />
-            <VideoContainer
-        avatarSrc={
-          "https://yt3.ggpht.com/lhqOqy7EZrOa9BBe9XKWafeH7kw3kfJy5QtDWdGvuu8tADalm3g2jDveboMu2USuCJqklgiirw=s68-c-k-c0x00ffffff-no-rj"
-        }
-        imgSrc={
-          "https://i.ytimg.com/vi/Tqjt0atjFv8/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBg7gFAHwYLtUz9d20AswZs7vtdrw"
-        }
-        title={
-          "리브 샌박 vs. 광동 | 매치15 하이라이트 | 01.21 | 2022 LCK 스프링 스플릿"
-        }
-        subscriber={
-          "LCK"
-        }
-
-        uploadTime={
-          "10분전"
-        }
-        views={
-          "조회수 816회"
-        }
-      />
-            <VideoContainer
-        avatarSrc={
-          "https://yt3.ggpht.com/lhqOqy7EZrOa9BBe9XKWafeH7kw3kfJy5QtDWdGvuu8tADalm3g2jDveboMu2USuCJqklgiirw=s68-c-k-c0x00ffffff-no-rj"
-        }
-        imgSrc={
-          "https://i.ytimg.com/vi/Tqjt0atjFv8/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBg7gFAHwYLtUz9d20AswZs7vtdrw"
-        }
-        title={
-          "리브 샌박 vs. 광동 | 매치15 하이라이트 | 01.21 | 2022 LCK 스프링 스플릿"
-        }
-        subscriber={
-          "LCK"
-        }
-
-        uploadTime={
-          "10분전"
-        }
-        views={
-          "조회수 816회"
-        }
-      />
-            <VideoContainer
-        avatarSrc={
-          "https://yt3.ggpht.com/lhqOqy7EZrOa9BBe9XKWafeH7kw3kfJy5QtDWdGvuu8tADalm3g2jDveboMu2USuCJqklgiirw=s68-c-k-c0x00ffffff-no-rj"
-        }
-        imgSrc={
-          "https://i.ytimg.com/vi/Tqjt0atjFv8/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBg7gFAHwYLtUz9d20AswZs7vtdrw"
-        }
-        title={
-          "리브 샌박 vs. 광동 | 매치15 하이라이트 | 01.21 | 2022 LCK 스프링 스플릿"
-        }
-        subscriber={
-          "LCK"
-        }
-
-        uploadTime={
-          "10분전"
-        }
-        views={
-          "조회수 816회"
-        }
-      />
-            <VideoContainer
-        avatarSrc={
-          "https://yt3.ggpht.com/lhqOqy7EZrOa9BBe9XKWafeH7kw3kfJy5QtDWdGvuu8tADalm3g2jDveboMu2USuCJqklgiirw=s68-c-k-c0x00ffffff-no-rj"
-        }
-        imgSrc={
-          "https://i.ytimg.com/vi/Tqjt0atjFv8/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBg7gFAHwYLtUz9d20AswZs7vtdrw"
-        }
-        title={
-          "리브 샌박 vs. 광동 | 매치15 하이라이트 | 01.21 | 2022 LCK 스프링 스플릿"
-        }
-        subscriber={
-          "LCK"
-        }
-
-        uploadTime={
-          "10분전"
-        }
-        views={
-          "조회수 816회"
-        }
-      />
-      
     </div>
   );
 }
